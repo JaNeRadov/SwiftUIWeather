9f2bf0df28ba19341a3f7f59d85269290130c3c8
Yuriy Zarubin <temisfucincrazy@icloud.com>
2023-04-21 14:47:07 +0300

    Initial Commit

---
+++ SwiftUIWeather/SwiftUIWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWeatherApp.swift
+//  SwiftUIWeather
+//
+//  Created by Jane Z. on 21.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
