5c66bdbf1c377e8c00b64abcaae4b7106e3fbdd8
Yuriy Zarubin <temisfucincrazy@icloud.com>
2023-04-21 17:02:46 +0300

    Создан экран логина

--- SwiftUIWeather/ContentView.swift
+++ SwiftUIWeather/ContentView.swift
@@ -6,21 +6,75 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
+    @State private var login = ""
+    @State private var password = ""
+    @State private var shouldShowLogo: Bool = true
+    
+    private let keyboardIsOnPublisher = Publishers.Merge(
+        NotificationCenter.default.publisher(for: UIResponder.keyboardWillChangeFrameNotification)
+            .map { _ in true },
+        NotificationCenter.default.publisher(for: UIResponder.keyboardWillHideNotification)
+            .map{ _ in false }
+    )
+        .removeDuplicates()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            GeometryReader { geometry in
+                Image("MainWeather")
+                    .resizable()
+                    .edgesIgnoringSafeArea(.all)
+                    .aspectRatio(contentMode: .fill)
+                    .frame(maxWidth: geometry.size.width, maxHeight:
+                            geometry.size.height)
+            }
+            ScrollView {
+                VStack {
+                    Text("Wether")
+                        .padding(.top, 32)
+                        .font(.largeTitle)
+                    HStack {
+                        Text("Login:")
+                        Spacer()
+                        TextField("", text: $login)
+                            .frame(maxWidth: 150)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                    }
+                    HStack {
+                        Text("Password: ")
+                        Spacer()
+                        SecureField("", text: $password)
+                            .frame(maxWidth: 150)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                    }
+                } .frame(maxWidth: 250)
+                Button(action: { print("Hello") }) {
+                    Text("Log in")
+                }
+                .padding(.top, 50)
+                .padding(.bottom, 20)
+                .disabled(login.isEmpty || password.isEmpty)
+            }
         }
-        .padding()
-    }
-}
+        .onReceive(keyboardIsOnPublisher) { isKeyboardOn in
+            withAnimation(Animation.easeInOut(duration: 0.5)) {
+                self.shouldShowLogo = !isKeyboardOn
+            }
+        }.onTapGesture {
+            UIApplication.shared.endEditing() }
+    } }
+
+extension UIApplication { func endEditing() {
+    sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+} }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+
